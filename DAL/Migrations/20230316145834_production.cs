b19bb89bc3d87cfcd038be2196e1e77877b768a7
Richard Reintal <reintalrichard@gmail.com>
2023-03-16 16:58:52 +0200

    added db migration

---
+++ DAL/Migrations/20230316145834_production.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class production : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Place = table.Column<string>(type: "TEXT", nullable: false),
+                    EventStartTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Comment = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ParticipantTypes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ParticipantTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PaymentMethodTypes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentMethodTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Participants",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    IsCompany = table.Column<bool>(type: "INTEGER", nullable: false),
+                    PaymentMethodTypeId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    ParticipantTypeId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    EventId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: true),
+                    LastName = table.Column<string>(type: "TEXT", nullable: true),
+                    SocialsecurityNumber = table.Column<string>(type: "TEXT", nullable: true),
+                    CorporationName = table.Column<string>(type: "TEXT", nullable: true),
+                    CorporationCode = table.Column<string>(type: "TEXT", nullable: true),
+                    CorporationParcitipationsCount = table.Column<int>(type: "INTEGER", nullable: true),
+                    Comment = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Participants", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Participants_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Participants_ParticipantTypes_ParticipantTypeId",
+                        column: x => x.ParticipantTypeId,
+                        principalTable: "ParticipantTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Participants_PaymentMethodTypes_PaymentMethodTypeId",
+                        column: x => x.PaymentMethodTypeId,
+                        principalTable: "PaymentMethodTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ParticipantTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("79164c20-5b6a-41fa-bcd5-c193b1c7f5dc"), "Juriidiline isik" },
+                    { new Guid("c0f58002-3130-45cf-98e7-3c1302dbd86a"), "Eraisik" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "PaymentMethodTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("0e34e9bf-0cc9-4cd9-99c5-a6f90133b18b"), "Ülekanne" },
+                    { new Guid("eed52e35-b6de-4704-b536-45892dc6413b"), "Sularaha" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Participants_EventId",
+                table: "Participants",
+                column: "EventId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Participants_ParticipantTypeId",
+                table: "Participants",
+                column: "ParticipantTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Participants_PaymentMethodTypeId",
+                table: "Participants",
+                column: "PaymentMethodTypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Participants");
+
+            migrationBuilder.DropTable(
+                name: "Events");
+
+            migrationBuilder.DropTable(
+                name: "ParticipantTypes");
+
+            migrationBuilder.DropTable(
+                name: "PaymentMethodTypes");
+        }
+    }
+}
